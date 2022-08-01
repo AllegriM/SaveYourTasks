bd4d3be8ea29aac19e29723120c31eeac2503dd6
AllegriM <elmonky99@gmail.com>
2022-08-01 15:38:06 -0300

    added dark mode default

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,6 +1,9 @@
 import { extendTheme } from "@chakra-ui/react"
 
 export const ToDoTheme = extendTheme({
+    config: {
+        initialColorMode: "dark"
+    },
     colors: {
         pink: {
             500: "#E66EB2"
